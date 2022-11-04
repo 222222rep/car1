d601cbe033f79cbcb2122a09ef08e69868000c60
222222rep <100673184+222222rep@users.noreply.github.com>
2022-11-04 09:03:23 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,6 @@
+const header_dop_window = document.querySelector('.header_dop_window');
+const dop_window = document.querySelector('.dop_window');
+
+header_dop_window.addEventListener('click', () => {
+  dop_window.classList.toggle('active');
+});
