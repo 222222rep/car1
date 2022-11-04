98dde5783855fef12b406c0e0bafe3a376907402
222222rep <100673184+222222rep@users.noreply.github.com>
2022-11-04 09:32:40 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -4,3 +4,8 @@ const dop_window = document.querySelector('.dop_window');
 header_dop_window.addEventListener('click', () => {
   dop_window.classList.toggle('active');
 });
+
+header_dop_window.addEventListener('click', () => {
+  dop_window.classList.toggle('active');
+  document.querySelector('.arr').classList.toggle('rotate');
+});
